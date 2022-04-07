a5bea01e48989756b6b90ca65ebdadc921f53dff
dileep kumar <63330310+dileep9490@users.noreply.github.com>
2022-04-07 11:15:46 +0530

    rename

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,20 +1,20 @@
-
 import 'package:finance_app_ui/hompage/views/homepage.dart';
 import 'package:finance_app_ui/login/views/login_page.dart';
-import 'package:finance_app_ui/transactions/views/transactions_page.dart';
+import 'package:finance_app_ui/transactions/views/recent_transactions_page.dart';
 import 'package:flutter/material.dart';
 
 class RouteManager {
   static Route<dynamic> generateRoute(RouteSettings settings) {
-    final args = settings.arguments;
+
 
     switch (settings.name) {
       case (HomePage.route):
         return MaterialPageRoute(builder: (context) => const HomePage());
 
-      case (TransactionPage.route):
-        return MaterialPageRoute(builder: (context) => const TransactionPage());
-        
+      case (RecentTransactionPage.route):
+        return MaterialPageRoute(
+            builder: (context) => const RecentTransactionPage());
+
       case (LoginPage.route):
       default:
         return MaterialPageRoute(builder: (context) => const LoginPage());
